9172f86e119844643e793428736d28d6021dc515
andrew <andynazarov1996@gmail.com>
2018-03-07 14:09:01 +0200

    sum

--- test1.js
+++ test1.js
@@ -1,20 +1,15 @@
-function pow(x, n) {
-  var result = x;
-
-  for (var i = 1; i < n; i++) {
-    result *= x;
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
   }
 
-  return result;
+function showOk() {
+  alert( "Вы согласились." );
 }
 
-var x = prompt("x?", '');
-var n = prompt("n?", '');
-
-if (n <= 1) {
-  alert('Степень ' + n +
-    'не поддерживается, введите целую степень, большую 1'
-  );
-} else {
-  alert( pow(x, n) );
+function showCancel() {
+  alert( "Вы отменили выполнение." );
 }
+
+// использование
+ask("Вы согласны?", showOk, showCancel);
